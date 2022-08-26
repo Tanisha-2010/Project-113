67438fc3dc58080880a4774efe721afc75ec5641
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-08-26 22:08:04 +0530

    Add files via upload
    framed

--- main.js
+++ main.js
@@ -3,21 +3,46 @@ function preload() {
 }
 
 function setup() {
-    canvas = createCanvas(640, 480);
-    canvas.position(300, 230);
+    canvas = createCanvas(640, 430);
+    canvas.position(300, 120);
     video = createCapture(VIDEO);
     video.hide();
-    tint_color = "";
 }
 
 function draw() {
     image(video, 0, 0, 640, 480);
-    tint(tint_color);
 
-}
+    fill(10, 200, 10);
+    stroke(0, 128, 0);
+    circle(605, 395, 70);
 
-function apply_tint() {
-    tint_color = document.getElementById("input_tint_color").value;
+    fill(10, 200, 10);
+    stroke(0, 128, 0);
+    circle(605, 35, 70);
+
+    fill(10, 200, 10);
+    stroke(0, 128, 0);
+    circle(35, 395, 70);
+
+    fill(10, 200, 10);
+    stroke(0, 128, 0);
+    circle(35, 35, 70);
+
+    fill(300, 0, 0);
+    stroke(0, 80, 0);
+    rect(70, 20, 505, 30);
+
+    fill(300, 0, 0);
+    stroke(0, 80, 0);
+    rect(70, 380, 505, 30);
+
+    fill(300, 0, 0);
+    stroke(0, 80, 0);
+    rect(20, 65, 30, 300);
+
+    fill(300, 0, 0);
+    stroke(0, 80, 0);
+    rect(590, 65, 30, 300);
 }
 
 function take_snapshot() {
